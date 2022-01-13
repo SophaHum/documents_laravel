9274d8fc0b13d6fb67dbbdc59dac8c84fa9380d7
Hum Sopha <65023605+SophaHum@users.noreply.github.com>
2022-01-13 12:05:25 +0700

    Create valet.php

---
+++ valet.php
@@ -0,0 +1,3 @@
+https://qirolab.com/posts/install-laravel-valet-linux-development-environment-on-ubuntu
+
+Now add the export PATH="$PATH:$HOME/.composer/vendor/bin" or export PATH="$PATH:$HOME/.config/composer/vendor/bin" to ~/.bash_profile or ~/.bashrc or ~/.zshrs.
