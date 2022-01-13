bc407409c55de74416425f45fc01f8a17f37abbd
Hum Sopha <65023605+SophaHum@users.noreply.github.com>
2022-01-13 13:54:29 +0700

    Create nginx.php

---
+++ nginx.php
@@ -0,0 +1,12 @@
+https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-20-04
+
+sudo apt update
+sudo apt install nginx
+sudo ufw app list
+sudo ufw status
+systemctl status nginx
+sudo systemctl stop nginx
+sudo systemctl start nginx
+sudo systemctl restart nginx
+sudo systemctl disable nginx
+sudo systemctl enable nginx
